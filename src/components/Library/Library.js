91740ed6006e62ce7db6ee11969aa9cd56220d4c
noorsystems <150002040+noorsystems@users.noreply.github.com>
2024-06-06 15:50:24 +0500

    add local server logic

--- src/components/Library/Library.js
+++ src/components/Library/Library.js
@@ -1,63 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import Song from '../SongItem/Song';
 import AddSongForm from '../AddSongForm/AddSongForm';
+import axios from 'axios';
 import './style.scss';
 
 const Library = () => {
   const [songs, setSongs] = useState([]);
 
-  let starterData = [
-    {
-      "name": "Midnight Echoes",
-      "prompt": "Reflections under the starlight",
-      "color": "red",
-      "createdAt": "2024-05-06T17:00:00-07:00"
-    },
-    {
-      "name": "Whispering Pines",
-      "prompt": "Mystic trees in twilight's glow",
-      "color": "green",
-      "createdAt": "2024-05-06T17:00:00-05:00"
-    },
-    {
-      "name": "Neon Dreams",
-      "prompt": "City lights in the rain",
-      "color": "blue",
-      "createdAt": "2024-05-06T17:00:00-06:00"
-    },
-    {
-      "name": "Ocean's Embrace",
-      "prompt": "Waves crashing on moonlit shores",
-      "color": "yellow",
-      "createdAt": "2024-05-06T17:00:00-02:00"
-    }
-  ]
-  
+  // fetching songs
+  useEffect(() => {
+    axios.get('http://localhost:3001/songList')
+      .then(response => {
+        setSongs(response.data);
+      })
+      .catch(error => {
+          console.error('Error fetching todos:', error);
+      });
+  }, []);
+ 
+  // 
   const getRandomColor = () => {
     const colors = ['red', 'green', 'blue', 'yellow', 'orange', 'pink', 'cyan', 'magenta'];
     const randomIndex = Math.floor(Math.random() * colors.length);
     return colors[randomIndex];
   };  
 
-  useEffect(() => {
-    // Fetch songs data from localStorage or use the starter data
-    const storedSongs = JSON.parse(localStorage.getItem('songs'));
-    if (storedSongs.length > 0) {
-      setSongs(storedSongs);
-    } else {
-      setSongs([...starterData]);
-    }
-  }, []);
-
-  const updateLocalStorage = (songs) => {
-    localStorage.setItem('songs', JSON.stringify(songs));
-  }
-
-  useEffect(() => {
-    // Save songs data to localStorage
-    // localStorage.setItem('songs', JSON.stringify(songs));
-    updateLocalStorage(songs)
-  }, [songs]);
 
   const handleCreateSong = (name, prompt) => {
     const newSong = {
@@ -66,8 +33,14 @@ const Library = () => {
       color: getRandomColor(),
       createdAt: new Date().toISOString(),
     };
-    updateLocalStorage(songs)
-    setSongs([newSong, ...songs]);
+
+    axios.post('http://localhost:3001/songList', newSong)
+      .then(response => {
+        setSongs([newSong, response.data]);
+      })
+      .catch(error => {
+          console.error('Error adding todo:', error);
+      });
   };
 
   const handleDeleteSong = (index) => {
